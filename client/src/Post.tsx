1328b90ec1e64c1fb636e440f38d86c1f0363d0c
MaxvanSchoonderwoerd <mvanschoonderwoerd@maqqie.nl>
2025-12-18 09:03:56 +0100

    add created and edited date, changed user to username

--- client/src/Post.tsx
+++ client/src/Post.tsx
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getPost } from "./api/getPost";
 import { putPost } from "./api/putPost";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 import "./App.css";
 
 type TPost = {
   id: string;
-  user: string;
+  username: string;
   title: string;
   body: string;
   active: string;
+  created_at: string;
+  edited_at: string;
 };
 
 function Post() {
   const [post, setPost] = useState<TPost>();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [createdAt, setCreatedAt] = useState(new Date());
+  const [editedAt, setEditedAt] = useState(new Date());
 
   type postParams = {
     postId: string | undefined;
@@ -26,7 +32,7 @@ function Post() {
   async function handleUpdatePost(e: React.FormEvent) {
     e.preventDefault();
     if (!postId || !post) return;
-    await putPost(post?.user, title, body, post?.active, postId);
+    await putPost(post?.username, title, body, post?.active, postId);
     setTitle("");
     setBody("");
     loadPost();
@@ -39,6 +45,8 @@ function Post() {
       setPost(response);
       setTitle(response.title);
       setBody(response.body);
+      setCreatedAt(new Date(response.createdAt));
+      setEditedAt(new Date(response.editedAt));
     } catch (error) {
       console.error(error);
     }
@@ -47,6 +55,9 @@ function Post() {
   useEffect(() => {
     loadPost();
   }, [postId]);
+
+  TimeAgo.addLocale(en);
+  const timeAgo = new TimeAgo("en-US");
 
   return (
     <section className="App">
@@ -58,13 +69,6 @@ function Post() {
         </h1>
       </nav>
       <div className="container">
-        {/* <div className="postContainer">
-          <div className="postDetails">
-            <h1 className="postContent">{post?.title}</h1>
-            <p className="postContent">By: {post?.user}</p>
-            <p className="postContent">{post?.body}</p>
-          </div>
-        </div> */}
         <div className="postContainer">
           <form className="postDetails" onSubmit={handleUpdatePost}>
             <input
@@ -78,6 +82,10 @@ function Post() {
                 setTitle(e.target.value);
               }}
             />
+            <p>
+              Created {timeAgo.format(createdAt)}, edited
+              {timeAgo.format(editedAt)}
+            </p>
             <textarea
               className="postDetailsBody"
               id="postBody"
