b5eca5be5b9b26c7af4d549904d64d797920d882
MaxvanSchoonderwoerd <mvanschoonderwoerd@maqqie.nl>
2025-12-19 10:12:58 +0100

    changed name to username

--- client/src/api/putPost.ts
+++ client/src/api/putPost.ts
@@ -1,11 +1,17 @@
 import { API_URL } from "./config";
 
-export async function putPost(user: string, title: string, body: string, active: string, postId: string) {
+export async function putPost(
+  username: string,
+  title: string,
+  body: string,
+  active: string,
+  postId: string
+) {
   return await fetch(`${API_URL}posts/${postId}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      user,
+      username,
       title,
       body,
       active,
